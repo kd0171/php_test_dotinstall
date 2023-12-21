0069cd95641e1c2f83f3300072b20a3addba7a67
kd0171 <kido.0117.1895@gmail.com>
2023-12-21 14:21:34 +0100

    データ型

--- test.php
+++ test.php
@@ -72,6 +72,54 @@ echo MYFAMILYNAME.PHP_EOL;
 const MYFIRSTNAME = 'KENTA';
 echo MYFIRSTNAME.PHP_EOL;
 
+$a = 'hello';
+$b = 10;
+$c = 1.3;
+$d = null;
+$e = true;
+// 型を確かめる
+var_dump($a);
+var_dump($b);
+var_dump($c);
+var_dump($d);
+var_dump($e);
+// 型変換（キャスト）
+$a = (float)10;
+$b = (string)1.3;
+var_dump($a);
+var_dump($b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
