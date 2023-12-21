7ccb56ea84a803db5647dffb57abc2951ac5809e
kd0171 <kido.0117.1895@gmail.com>
2023-12-21 14:02:39 +0100

    複数行を変数に代入：終端記号の使い方

--- test.php
+++ test.php
@@ -26,5 +26,26 @@ echo "It's \"me\". Hello $name".PHP_EOL;
 //特殊文字：タブ
 echo "It's me.\tHello $name".PHP_EOL;
 
+//<<<'EOT' 改行、 EOT; としてあげると、ここに書いたテキストの改行や字下げは保持したまま、 $text という変数に代入してくれる
+    // $text = <<<'EOT'
+    // hello!
+    //  this is a long text
+    // Goodbye!
+    // EOT;
+    // echo $text;
+
+// ダブルクオーテーションまたは終端記号のみだと変数を代入可能になる
+    // $text = <<<"EOT"
+$text = <<<EOT
+hello!
+ this is $name
+Goodbye!
+EOT;
+
+echo $text;
+
+
+
+
 //終了タグ　PHP のファイルには HTML も書けますが、 PHP のコードしか書かない場合は閉じタグを書くべきではないというルールに
 ?>
