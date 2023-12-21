f35c78a4c7a9e8bb9fd38073e6247a2cf1d2a8fb
kd0171 <kido.0117.1895@gmail.com>
2023-12-21 13:43:30 +0100

    変数の使い方

--- test.php
+++ test.php
@@ -1,3 +1,19 @@
-<?php
 
-echo 'こんにちは'.PHP_EOL;
+<?php
+//開始タグ
+
+
+// コメントアウト　行頭：　//  #  複数行： /* */    ショートカット：ctrl+/
+
+// 変数の使い方
+$name = 'KENTA';
+
+//PHP_EOL; というキーワードを使ってあげれば、環境に応じて適切に改行
+// echo 'Hello Kenta'.PHP_EOL;
+// echo 'Hello Kido'.PHP_EOL;
+
+echo 'Hello '.$name.PHP_EOL;
+
+
+//終了タグ　PHP のファイルには HTML も書けますが、 PHP のコードしか書かない場合は閉じタグを書くべきではないというルールに
+?>
