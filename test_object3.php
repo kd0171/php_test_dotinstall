f010d29b55b0bb4e4e29b711f398ac39562b969d
kd0171 <kido.0117.1895@gmail.com>
2023-12-23 14:02:14 +0100

    クラスの継承

---
+++ test_object3.php
@@ -0,0 +1,30 @@
+<?php
+
+class Post // 親クラス Superクラス
+{
+  private $text;
+
+  public function __construct($text)
+  {
+    $this->text = $text;
+  }
+
+  public function show()
+  {
+    printf('%s' . PHP_EOL, $this->text);
+  }
+}
+
+class SponsoredPost extends Post // 子クラス Subクラス
+{
+  
+}
+
+$posts = [];
+$posts[0] = new Post('hello');
+$posts[1] = new Post('hello again');
+$posts[2] = new SponsoredPost('Sub: hello hello');
+
+$posts[0]->show();
+$posts[1]->show();
+$posts[2]->show();
