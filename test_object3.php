0a03d23cc14c98620c547642f0d5b66033393508
kd0171 <kido.0117.1895@gmail.com>
2023-12-23 14:05:09 +0100

    子クラスで独自のプロパティ、メソッド 親クラスのコンストラクタの使用：parent::__construct($text);

--- test_object3.php
+++ test_object3.php
@@ -17,14 +17,35 @@ class Post // 親クラス Superクラス
 
 class SponsoredPost extends Post // 子クラス Subクラス
 {
-  
-}
+    // 子クラスの独自のプロパティ
+    private $sponsor;
+    
+    public function __construct($text, $sponsor)
+    {
+    // 親クラスのコンストラクタを使う
+      parent::__construct($text);
+      $this->sponsor = $sponsor;
+    }
+    
+    public function showSponsor()
+    {
+      printf('%s' . PHP_EOL, $this->sponsor);
+    }
+  }
 
 $posts = [];
 $posts[0] = new Post('hello');
 $posts[1] = new Post('hello again');
-$posts[2] = new SponsoredPost('Sub: hello hello');
+$posts[2] = new SponsoredPost('Sub: hello hello', 'dotinstall');
 
 $posts[0]->show();
 $posts[1]->show();
 $posts[2]->show();
+$posts[2]->showSponsor();
+
+
+
+
+
+
+
