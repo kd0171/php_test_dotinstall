0be340295b7048973a327dc50778b0d598eb5f7c
kd0171 <kido.0117.1895@gmail.com>
2023-12-23 12:52:35 +0100

    インスタンス化、代入、関数の使用

--- test_object.php
+++ test_object.php
@@ -1,5 +1,23 @@
 <?php
 
+
+echo "投稿データ作成：　". PHP_EOL;
+$postsalt = [];
+$postsalt[0] = ['text' => 'hello', 'likes' => 0];
+$postsalt[1] = ['text' => 'hello again', 'likes' => 0];
+
+print_r($postsalt);
+
+function show($postsalt)
+{
+// 最初の%sは文字列(=$post['text'])、次の%dは整数(=$post['likes'])
+  printf('%s (%d)' . PHP_EOL, $postsalt['text'], $postsalt['likes']);
+}
+
+show($postsalt[0]);
+show($postsalt[1]);
+
+
 // クラス
 class Post
 {
@@ -15,21 +33,30 @@ class Post
   }
 }
 
-echo "投稿データ作成：　". PHP_EOL;
 $posts = [];
-$posts[0] = ['text' => 'hello', 'likes' => 0];
-$posts[1] = ['text' => 'hello again', 'likes' => 0];
+// $posts[0] = ['text' => 'hello', 'likes' => 0];
+$posts[0] = new Post();
+$posts[0]->text = 'hello';
+$posts[0]->likes = 0;
+// $posts[1] = ['text' => 'hello again', 'likes' => 0];
+$posts[1] = new Post();
+$posts[1]->text = 'hello again';
+$posts[1]->likes = 0;
 
-print_r($posts);
+// show($posts[0]);
+// show($posts[1]);
+$posts[0]->show();
+$posts[1]->show();
 
-function show($post)
-{
-// 最初の%sは文字列(=$post['text'])、次の%dは整数(=$post['likes'])
-  printf('%s (%d)' . PHP_EOL, $post['text'], $post['likes']);
-}
 
-show($posts[0]);
-show($posts[1]);
+
+
+
+
+
+
+
+
 
 
 
